53aa4a048beacab34103e8303b76414a05ac0766
MehdiGhafourian <mehdi.ghafourian@yahoo.com>
2026-07-21 06:50:46 +0330

    Add docker compose and database migration

--- Evently/src/API/Evently.Api/Program.cs
+++ Evently/src/API/Evently.Api/Program.cs
@@ -1,20 +1,28 @@
+using Evently.Api.Extensions;
+using Evently.Modules.Events.Api;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddEventsModule(builder.Configuration);
+
 WebApplication app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger(); 
+    app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
 
+EventsModule.MapEndpoints(app);
 
 app.Run();
 
