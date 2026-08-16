f9d4d53c9a598532a488d0e7513ea8197a7d16af
MehdiGhafourian <mehdi.ghafourian@yahoo.com>
2026-08-16 19:21:25 +0330

    Add module configuration settings

--- Evently/src/API/Evently.Api/Program.cs
+++ Evently/src/API/Evently.Api/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddApplication([Evently.Modules.Events.Application.AssemblyRefe
 
 builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("Database")!);
 
+builder.Configuration.AddModuleConfiguration(["events"]);
+
 builder.Services.AddEventsModule(builder.Configuration);
 
 WebApplication app = builder.Build();
