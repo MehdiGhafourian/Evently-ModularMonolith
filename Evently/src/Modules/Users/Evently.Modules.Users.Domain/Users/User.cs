147a70074d142cee6c4155e8f22158a85d92d8a9
MehdiGhafourian <mehdi.ghafourian@yahoo.com>
2026-09-24 18:53:11 +0330

    Create User Module and develop Domain Layer

---
+++ Evently/src/Modules/Users/Evently.Modules.Users.Domain/Users/User.cs
@@ -0,0 +1,55 @@
+﻿using Evently.Common.Domain;
+
+namespace Evently.Modules.Users.Domain.Users;
+
+public sealed class User : Entity
+{
+    private User()
+    {
+
+    }
+
+    public Guid Id { get; private set; }
+
+    public string Email { get; private set; }
+
+    public string FirstName { get; private set; }
+
+    public string LastName { get; private set; }
+
+    public static User Create(string email, string firstName, string lastName)
+    {
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            Email = email,
+            FirstName = firstName,
+            LastName = lastName
+        };
+
+        user.Raise(new UserRaisedDomainEvent(user.Id));
+
+        return user;
+    }
+
+    public void Update(string firstName, string lastName)
+    {
+        if (FirstName == firstName && LastName == lastName)
+        {
+            return;
+        }
+
+        FirstName = firstName;
+        LastName = lastName;
+
+        Raise(new UserProfileUpdatedDomainEvent(Id, FirstName, LastName));
+    }
+}
+
+public sealed class UserProfileUpdatedDomainEvent(Guid userId, string firstName, string lastName) : DomainEvent
+{
+    public Guid UserId { get; init; } = userId;
+    public string FirstName { get; init; } = firstName;
+    public string LastName { get; init; } = lastName;
+}
+}
